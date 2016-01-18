88dbfaf5d61dd949ed4f16ede19bd93b791644ba
SeanKennedy12 <seankennedy@seans-air-2.home>
2016-01-18 19:46:28 +0000

    Added example exercise with basic functionality to mark questions. Layout not correct on phone, works on computers.

---
+++ Exercises.js
@@ -0,0 +1,9 @@
+function markQuestions() {
+	var answer1 = document.getElementById("answer1").value;
+	if (answer1 == 6) {
+		alert("Well done!!! You got 1/1 Correct");	
+	}
+	else {
+		alert("You got 0/1 correct, please try again");
+	}
+}
