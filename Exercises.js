64e27c69af928324cdec7ac8fcb2327c2ef93bb0
SeanKennedy12 <seankennedy@seans-air-2.home>
2016-01-18 21:52:18 +0000

    Questions can now be answered and marked giving the user a score for the exercise

--- Exercises.js
+++ Exercises.js
@@ -1,14 +1,36 @@
-function markQuestions() {
+function markQuestions(questionOrder) {
+	this.questionOrder = questionOrder;
+	
+	var noOfQuestions = questionOrder.length;
+	var score = 0;
+	//keeps track of what questions are answered incorrectly
+	var wrongQuestions = [1, 2];
+	
+	var questionAnswers = [6, 2, 01];
+	
 	var answer1 = document.getElementById("answer1").value;
 	var answer2 = document.getElementById("answer2").value;
 	
-	if (answer1 == 6 && answer2 == 2) {
-		alert("Well done!!! You got 2/2 Correct");	
+	console.log(answer1);
+	console.log(questionAnswers[questionOrder[0]]);
+	
+	if(answer1 == questionAnswers[questionOrder[0]]) {
+		score += 1;
+		//array.splice(0, 1) removes the element at position 0
+		wrongQuestions.splice(0, 1);
 	}
-	else if (answer1 == 6 || answer2 ==2) {
-		alert("You got 1/2 correct, please try again");
+	if(answer2 == questionAnswers[questionOrder[1]]) {
+		score += 1;
+		wrongQuestions.splice(1, 1);
+	}
+	
+	if (score == noOfQuestions) {
+		alert("You Scored " + score + "/" + noOfQuestions + ". Well Done!");
+	}
+	else if(score == noOfQuestions - 1) {
+		alert("You Scored " + score + "/" + noOfQuestions + ". You answered question " + wrongQuestions + " incorrectly");
 	}
 	else {
-		alert ("You got 0/2 correct, please try again");
+		alert("You Scored " + score + "/" + noOfQuestions + ". You answered questions " + wrongQuestions + " incorrectly");
 	}
 }
--- NestedIfQuestions1.js
+++ NestedIfQuestions1.js
@@ -1,6 +1,6 @@
 var questions = ["How many times would the following segment of code output the word \"Hello\"?" + "<br>" + "<pre>" + "for (int i = 0; i < 3; i++) {" + "<br>	for (int j = 0; j < 2; j++) {" + "<br>		System.out.println(\"Hello\");" + "<br>	}" + "<br>}" + "</pre>",
 "What value would x have to be assigned to to output the word \"World\" 8 times?" + "<br>" + "<pre>" + "int x = ?" +"<br>for (int i = 0; i < x; i++) {" + "<br>	for (int j = 0; j < 4; j++) {" + "<br>		System.out.println(\"World\");" + "<br>	}" + "<br>}" + "</pre>",
-"What is the output of the following code fragement? <br>" + "<pre>" + "for (int a = 0; a <2; a++) {" + "<br>	for (int b = 0; b < 3; b++) { " + "<br>	System.out.println(a);" + "<br>	}" + "<br>}" + "</pre>"];
+"What is the output of the following code fragement? <br>" + "<pre>" + "for (int a = 0; a <2; a++) {" + "<br>	for (int b = 0; b < 3; b++) { " + "<br>	System.out.print(a);" + "<br>	}" + "<br>}" + "</pre>"];
 
 
 
