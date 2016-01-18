7d4cee6f966b1dfd9f2482e5621b4a47da0f9ef6
SeanKennedy12 <seankennedy@seans-air-2.home>
2016-01-18 21:19:44 +0000

    Questions are now randomly chosen from question bank. Answers are not yet coded

--- Exercises.js
+++ Exercises.js
@@ -1,9 +1,14 @@
 function markQuestions() {
 	var answer1 = document.getElementById("answer1").value;
-	if (answer1 == 6) {
-		alert("Well done!!! You got 1/1 Correct");	
+	var answer2 = document.getElementById("answer2").value;
+	
+	if (answer1 == 6 && answer2 == 2) {
+		alert("Well done!!! You got 2/2 Correct");	
+	}
+	else if (answer1 == 6 || answer2 ==2) {
+		alert("You got 1/2 correct, please try again");
 	}
 	else {
-		alert("You got 0/1 correct, please try again");
+		alert ("You got 0/2 correct, please try again");
 	}
 }
---
+++ NestedIfQuestions1.js
@@ -0,0 +1,11 @@
+var questions = ["How many times would the following segment of code output the word \"Hello\"?" + "<br>" + "<pre>" + "for (int i = 0; i < 3; i++) {" + "<br>	for (int j = 0; j < 2; j++) {" + "<br>		System.out.println(\"Hello\");" + "<br>	}" + "<br>}" + "</pre>",
+"What value would x have to be assigned to to output the word \"World\" 8 times?" + "<br>" + "<pre>" + "int x = ?" +"<br>for (int i = 0; i < x; i++) {" + "<br>	for (int j = 0; j < 4; j++) {" + "<br>		System.out.println(\"World\");" + "<br>	}" + "<br>}" + "</pre>",
+"What is the output of the following code fragement? <br>" + "<pre>" + "for (int a = 0; a <2; a++) {" + "<br>	for (int b = 0; b < 3; b++) { " + "<br>	System.out.println(a);" + "<br>	}" + "<br>}" + "</pre>"];
+
+
+
+/*
+more examples to incude, possibly in harder exercises.
+for (int a = 1; a < 5; a++)
+for (int z = 4; z > 2; z++)
+*/
